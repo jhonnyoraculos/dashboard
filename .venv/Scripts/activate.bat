53b173631e0e270345e00acb11316c71734248b6
jhonnyoraculos <jhgamer120@gmail.com>
2025-11-07 11:58:32 -0300

    Atualiza├º├úo autom├ítica 2025-11-07 11:58

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jrferragens\OneDrive - JR Ferragens & Madeiras\01\HTML\DASHBOARD COMBUSTIVEL\DASHBOARD\.venv
+set VIRTUAL_ENV=C:\Users\jrferragens\OneDrive - JR Ferragens & Madeiras\01\HTML\DASHBOARD\DASHBOARD\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
